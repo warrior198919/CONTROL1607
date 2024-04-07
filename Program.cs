fd836199f0797e2b67a28ca400105bc6c19084b3
Aleksey Graunov <agraunov@gmail.com>
2024-04-07 13:03:35 +0300

    Job leter

--- Program.cs
+++ Program.cs
@@ -1,9 +1,1 @@
-﻿// код программы «Привет, мир!»
-namespace HelloWorld
-// {
-//     class Hello {         
-//         static void Main(string[] args)
-//         {
-//             System.Console.WriteLine("Привет, мир!");
-//         }
-//
+﻿// Hello Word C#
