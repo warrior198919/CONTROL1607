24f30b29fa97b104dc193a1ca2bf3483f4001ad8
Aleksey Graunov <agraunov@gmail.com>
2024-04-07 12:48:33 +0300

    repair

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// код программы «Привет, мир!»
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Привет, мир!");
+        }
+    }
+}
