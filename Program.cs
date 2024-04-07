d736cc9f63bb54c0f2d1961092192b6f5c438760
Aleksey Graunov <agraunov@gmail.com>
2024-04-07 14:48:57 +0300

    new cod

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
 ﻿// Hello Word C#
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк: ");
+        int n;
+        if (!int.TryParse(Console.ReadLine(), out n))
+        {
+            Console.WriteLine("Ошибка ввода. Пожалуйста, введите целое число.");
+            return;
+        }
+
+        string[] inputArr = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите строку {i + 1}: ");
+            inputArr[i] = Console.ReadLine();
+        }
+
+        string[] resultArr = inputArr.Where(str => str.Length <= 3).ToArray();
+
+        Console.WriteLine("Результат:");
+        foreach (string str in resultArr)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
